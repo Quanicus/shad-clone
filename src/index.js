9c0b15f9432016bdb1f45577bd5f419a4fdda50b
Quan Pham <quanicus@Quans-MacBook-Pro-2.local>
2024-07-27 16:00:45 -0500

    test just one component and fixing form clear

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import "./components/shad-toggle.js";
-import "./components/shad-button.js";
+// import "./components/shad-toggle.js";
+// import "./components/shad-button.js";
 import "./components/shad-input-text.js";
-import "./components/shad-tooltip.js";
-import "./components/scroll-bar.js"
+// import "./components/shad-tooltip.js";
+// import "./components/scroll-bar.js"
